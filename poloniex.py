446f11bded237f4a9804faa9dbe39bbfd36e22d3
Rasmus Nordström <nordstrom.rasmus@gmail.com>
2016-01-28 19:18:11 +0100

    Comments fixed

--- poloniex.py
+++ poloniex.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import urllib
 import urllib2
 import json
@@ -5,10 +7,12 @@ import time
 import hmac,hashlib
 
 def createTimeStamp(datestr, format="%Y-%m-%d %H:%M:%S"):
+    """Generates as timestamp"""
     return time.mktime(time.strptime(datestr, format))
 
 class poloniex:
     def __init__(self, APIKey, Secret):
+        """Get your keys from poloniex.com"""
         self.APIKey = APIKey
         self.Secret = Secret
 
@@ -51,7 +55,6 @@ class poloniex:
             jsonRet = json.loads(ret.read())
             return self.post_process(jsonRet)
 
-
     def returnTicker(self):
         return self.api_query("returnTicker")
 
@@ -64,73 +67,98 @@ class poloniex:
     def returnMarketTradeHistory (self, currencyPair):
         return self.api_query("returnMarketTradeHistory", {'currencyPair': currencyPair})
 
-
-    # Returns all of your balances.
-    # Outputs: 
-    # {"BTC":"0.59098578","LTC":"3.31117268", ... }
     def returnBalances(self):
+        """Returns all of your balances.
+          Outputs: 
+         {"BTC":"0.59098578","LTC":"3.31117268", ... }
+         """
         return self.api_query('returnBalances')
 
-    # Returns your open orders for a given market, specified by the "currencyPair" POST parameter, e.g. "BTC_XCP"
-    # Inputs:
-    # currencyPair  The currency pair e.g. "BTC_XCP"
-    # Outputs: 
-    # orderNumber   The order number
-    # type          sell or buy
-    # rate          Price the order is selling or buying at
-    # Amount        Quantity of order
-    # total         Total value of order (price * quantity)
     def returnOpenOrders(self,currencyPair):
+        """Returns your open orders for a given market, specified by the 
+        "currencyPair" POST parameter, e.g. "BTC_XCP"
+ 
+        Inputs:
+        currencyPair  The currency pair e.g. "BTC_XCP"
+ 
+        Outputs: 
+        orderNumber   The order number
+        type          sell or buy
+        rate          Price the order is selling or buying at
+        Amount        Quantity of order
+        total         Total value of order (price * quantity)"""
         return self.api_query('returnOpenOrders',{"currencyPair":currencyPair})
-
-
-    # Returns your trade history for a given market, specified by the "currencyPair" POST parameter
-    # Inputs:
-    # currencyPair  The currency pair e.g. "BTC_XCP"
-    # Outputs: 
-    # date          Date in the form: "2014-02-19 03:44:59"
-    # rate          Price the order is selling or buying at
-    # amount        Quantity of order
-    # total         Total value of order (price * quantity)
-    # type          sell or buy
+        
     def returnTradeHistory(self,currencyPair):
+        """
+        Returns your trade history for a given market, specified by the 
+        "currencyPair" POST parameter
+ 
+        Inputs:
+        currencyPair  The currency pair e.g. "BTC_XCP"
+ 
+        Outputs: 
+        date          Date in the form: "2014-02-19 03:44:59"
+        rate          Price the order is selling or buying at
+        amount        Quantity of order
+        total         Total value of order (price * quantity)
+        type          sell or buy"""
+            
         return self.api_query('returnTradeHistory',{"currencyPair":currencyPair})
 
-    # Places a buy order in a given market. Required POST parameters are "currencyPair", "rate", and "amount". If successful, the method will return the order number.
-    # Inputs:
-    # currencyPair  The curreny pair
-    # rate          price the order is buying at
-    # amount        Amount of coins to buy
-    # Outputs: 
-    # orderNumber   The order number
     def buy(self,currencyPair,rate,amount):
+        """
+        Places a buy order in a given market. Required POST parameters are 
+        "currencyPair", "rate", and "amount". 
+        If successful, the method will return the order number.
+        
+        Inputs:
+        currencyPair  The curreny pair
+        rate          price the order is buying at
+        amount        Amount of coins to buy
+        
+        Outputs: 
+        orderNumber   The order number"""
         return self.api_query('buy',{"currencyPair":currencyPair,"rate":rate,"amount":amount})
 
-    # Places a sell order in a given market. Required POST parameters are "currencyPair", "rate", and "amount". If successful, the method will return the order number.
-    # Inputs:
-    # currencyPair  The curreny pair
-    # rate          price the order is selling at
-    # amount        Amount of coins to sell
-    # Outputs: 
-    # orderNumber   The order number
     def sell(self,currencyPair,rate,amount):
+        """
+        Places a sell order in a given market. Required POST parameters are 
+        "currencyPair", "rate", and "amount". 
+        If successful, the method will return the order number.
+        
+        Inputs:
+        currencyPair  The curreny pair
+        rate          price the order is selling at
+        amount        Amount of coins to sell
+        
+        Outputs: 
+        orderNumber   The order number"""
         return self.api_query('sell',{"currencyPair":currencyPair,"rate":rate,"amount":amount})
 
-    # Cancels an order you have placed in a given market. Required POST parameters are "currencyPair" and "orderNumber".
-    # Inputs:
-    # currencyPair  The curreny pair
-    # orderNumber   The order number to cancel
-    # Outputs: 
-    # succes        1 or 0
     def cancel(self,currencyPair,orderNumber):
+        """"
+        Cancels an order you have placed in a given market. Required POST parameters are "currencyPair" and "orderNumber".
+        Inputs:
+        currencyPair  The curreny pair
+        orderNumber   The order number to cancel
+        Outputs: 
+        succes        1 or 0
+        """
         return self.api_query('cancelOrder',{"currencyPair":currencyPair,"orderNumber":orderNumber})
-
-    # Immediately places a withdrawal for a given currency, with no email confirmation. In order to use this method, the withdrawal privilege must be enabled for your API key. Required POST parameters are "currency", "amount", and "address". Sample output: {"response":"Withdrew 2398 NXT."} 
-    # Inputs:
-    # currency      The currency to withdraw
-    # amount        The amount of this coin to withdraw
-    # address       The withdrawal address
-    # Outputs: 
-    # response      Text containing message about the withdrawal
+    
     def withdraw(self, currency, amount, address):
+        """
+        Immediately places a withdrawal for a given currency, with no email confirmation. 
+        In order to use this method, the withdrawal privilege must be enabled for your API key. 
+        Required POST parameters are "currency", "amount", and 
+        "address". Sample output: {"response":"Withdrew 2398 NXT."} 
+      
+        Inputs:
+        currency      The currency to withdraw
+        amount        The amount of this coin to withdraw
+        address       The withdrawal address
+        Outputs: 
+        response      Text containing message about the withdrawal"""
+        
         return self.api_query('withdraw',{"currency":currency, "amount":amount, "address":address})
